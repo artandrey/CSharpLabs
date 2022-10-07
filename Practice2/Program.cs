839b6dfb1ddc9ab13eeb1a3a8c0744e17a9c5553
Artandrey <artandrey7777@gmail.com>
2022-10-07 14:39:11 +0300

    added practice 2

---
+++ Practice2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace The2ndPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nn;
+            int nk;
+
+
+            double sum = 0;
+
+            do
+            {
+                Console.WriteLine("NN");
+                nn = int.Parse(Console.ReadLine());
+            } while (nn <= 0);
+
+
+            do
+            {
+                Console.WriteLine("NK");
+                nk = int.Parse(Console.ReadLine());
+            } while (nn >= nk);
+
+
+
+            for (int k = nn; k < nk; k++)
+            {
+                sum += (Math.Pow(k, 2) + Math.Pow(-1, (k - 1)) * 2 * k - 1) / (Math.Pow(k, 2) + 8);
+            }
+
+            Console.WriteLine(sum);
+
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+
+}
+ 
+
